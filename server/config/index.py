3dc3707d6e09cf7537bc25e99878d01fa8f94b32
Vepsian <moto2995@yandex.ru>
2019-04-29 18:07:04 +0300

    123

--- server/config/index.py
+++
@@ -1,6 +0,0 @@
-import xlrd, xlwt
-#открываем файл
-rb = xlrd.open_workbook('/home/data-scientist/Desktop/server/config/TRMZ_2x2_Alarms.xlsxTRMZ_2x2_Alarms.xlsx',formatting_info=True)
-
-#выбираем активный лист
-sheet = rb.sheet_by_index(0)
--- server/config/testtest.js
+++
@@ -1,4 +0,0 @@
-var fs = require("fs");
-var contents = fs.readFileSync("TRMZ_crane_2x2_Drives.json");
- var jsonContent = JSON.parse(contents);
- console.log(jsonContent);
